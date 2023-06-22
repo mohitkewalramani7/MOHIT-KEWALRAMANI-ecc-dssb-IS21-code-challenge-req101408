bffa6b86a4b15415e962820b0926d1201a298ef4
Mohit Kewalramani <mohitkewalramani74@gmail.com>
2023-06-22 13:39:45 -0700

    Set up mock data and connected server to front-end

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const mock_data = require('./data.json')
+var corsOptions = {
+  origin: 'http://localhost:3001'
+}
+
+app.get('/', cors(corsOptions), (req, res) => {
+  res.send(mock_data)
 })
 
 app.listen(port, () => {
--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -1,12 +1,27 @@
 'use client'
+import { useEffect, useState } from 'react'
 import styles from './projects.module.css'
 
 export default function Projects() {
+
+  const [projectData, setProjectData] = useState([])
+
+  useEffect(() => {
+    async function fetchProjectData() {
+      let res = await fetch('http://localhost:3000')
+      res = await res.json()
+      console.log(res)
+      //setProjectData(res)
+    }
+    fetchProjectData()
+  })
+
   return (
     <main className={styles.main}>
       <p className={styles.heading}>
         Projects by the ECC
       </p>
+      {projectData}
     </main>
   )
 }
